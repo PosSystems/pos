181c15809ea064b0fb8d5f75e7b1a7cf5f70bbce
Dominik Schneider <schn3d0@gmail.com>
2016-11-27 15:55:40 +0100

    implemented Employee model object with hibernate mapping

---
+++ pos/src/main/java/com/pos/system/model/Employee.java
@@ -0,0 +1,77 @@
+package com.pos.system.model;
+
+public class Employee {
+	private int intID;
+	private String strGivenName;
+	private String strSurname;
+	private String strStreet;
+	private int intPostCode;
+	private String strCity;
+	private String strPassword;
+	private boolean bitManagementArea;
+
+	public int getIntID() {
+		return intID;
+	}
+
+	public void setIntID(int intID) {
+		this.intID = intID;
+	}
+
+	public String getStrGivenName() {
+		return strGivenName;
+	}
+
+	public void setStrGivenName(String strGivenName) {
+		this.strGivenName = strGivenName;
+	}
+
+	public String getStrSurname() {
+		return strSurname;
+	}
+
+	public void setStrSurname(String strSurname) {
+		this.strSurname = strSurname;
+	}
+
+	public String getStrStreet() {
+		return strStreet;
+	}
+
+	public void setStrStreet(String strStreet) {
+		this.strStreet = strStreet;
+	}
+
+	public int getIntPostCode() {
+		return intPostCode;
+	}
+
+	public void setIntPostCode(int intPostCode) {
+		this.intPostCode = intPostCode;
+	}
+
+	public String getStrCity() {
+		return strCity;
+	}
+
+	public void setStrCity(String strCity) {
+		this.strCity = strCity;
+	}
+
+	public String getStrPassword() {
+		return strPassword;
+	}
+
+	public void setStrPassword(String strPassword) {
+		this.strPassword = strPassword;
+	}
+
+	public boolean isBitManagementArea() {
+		return bitManagementArea;
+	}
+
+	public void setBitManagementArea(boolean bitManagementArea) {
+		this.bitManagementArea = bitManagementArea;
+	}
+
+}
