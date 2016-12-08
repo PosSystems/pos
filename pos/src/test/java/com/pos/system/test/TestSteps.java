7b5813c21d0e47e78bbe4c68aaf91e6148c2ef08
Dominik Schneider <schn3d0@gmail.com>
2016-12-08 07:09:04 +0100

    implemented selling process feature test

--- pos/src/test/java/com/pos/system/test/TestSteps.java
+++ pos/src/test/java/com/pos/system/test/TestSteps.java
@@ -1,30 +1,53 @@
 package com.pos.system.test;
 
+import static org.junit.Assert.fail;
+
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.DataTable;
 import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class TestSteps {
 	
-	private WebDriver driver = null;
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
+	private int timeout = 30;
+
+	@Before
+	public void setUp() throws Exception {
+		driver = new ChromeDriver();
+		// driver = new MarionetteDriver(capabilities);
+		baseUrl = "http://localhost:8080/";
+		driver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
 
 	@Given("^user has identified as cashier$")
 	public void user_has_identified_as_cashier() throws Throwable {
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		// throw new PendingException();
+		throw new PendingException();
 	}
 
 	@When("^cashier opens the cash area the first time a day$")
 	public void cashier_opens_the_cash_area_the_first_time_a_day() throws Throwable {
-		driver.get("http://localhost:8080/system/faces/pos.xhtml");
 		throw new PendingException();
 	}
 
@@ -148,24 +171,66 @@ public class TestSteps {
 		throw new PendingException();
 	}
 
-	/*
-	 * @Given("^user has chrome running$") public void user_has_chrome_running()
-	 * throws Throwable { driver = new ChromeDriver();
-	 * driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); }
-	 * 
-	 * @When("^user navigates to hompepage$") public void
-	 * user_navigates_to_hompepage() throws Throwable {
-	 * driver.get("http://localhost:8080/system/"); }
-	 * 
-	 * @Then("^message contains Hello world!$") public void
-	 * message_contains_Hello_world() throws Throwable { if
-	 * (driver.getPageSource().contains("Hello world!")) { driver.quit();
-	 * return; } driver.quit(); throw new Throwable(); }
-	 * 
-	 * @Given("^user has Firefox running$") public void
-	 * user_has_Firefox_running() throws Throwable { driver = new
-	 * FirefoxDriver(); driver.manage().timeouts().implicitlyWait(10,
-	 * TimeUnit.SECONDS); }
-	 */
+	@Given("^cashier is on the cash area site$")
+	public void cashier_is_on_the_cash_area_site() throws Throwable {
+		driver.get((baseUrl + "system/faces/pos.xhtml"));
+	}
 
+	@When("^cashier clicks on a product group button$")
+	public void cashier_clicks_on_a_product_group_button() throws Throwable {
+		driver.findElement(By.id("cash:j_idt20:2:j_idt21")).click();
+	}
+
+	@When("^cashier enters (\\d+)\\.(\\d+) in the opened dialog$")
+	public void cashier_enters_in_the_opened_dialog(int arg1, int arg2) throws Throwable {
+		driver.findElement(By.id("j_idt32:priceInput")).clear();
+		driver.findElement(By.id("j_idt32:priceInput")).sendKeys("10.0");
+		driver.findElement(By.id("j_idt32:j_idt34")).click();
+	}
+
+	@Then("^the article is shown in the list with the price of (\\d+)\\.(\\d+)$")
+	public void the_article_is_shown_in_the_lis_with_the_price_of(int arg1, int arg2) throws Throwable {
+		if ("Speicherkarten".equals(driver.findElement(By.id("cash:j_idt24:0:j_idt28")).getText())) {
+			if ("10.0".equals(driver.findElement(By.id("cash:j_idt24:0:j_idt30")).getText())) {
+				return;
+			}
+		}
+		throw new Throwable();
+	}
+
+	@Given("^cashier has entered price for product group$")
+	public void cashier_has_entered_price_for_product_group() throws Throwable {
+		cashier_is_on_the_cash_area_site();
+		cashier_clicks_on_a_product_group_button();
+		cashier_enters_in_the_opened_dialog(10, 0);
+		the_article_is_shown_in_the_lis_with_the_price_of(10, 0);
+	}
+
+	@When("^cashier clicks on payment button$")
+	public void cashier_clicks_on_payment_button() throws Throwable {
+		driver.findElement(By.id("cash:j_idt23")).click();
+	}
+
+	@Then("^a dialog containing the difference in payment appears$")
+	public void a_dialog_containing_the_difference_in_payment_appears() throws Throwable {
+		driver.findElement(By.id("frmDlgDrawback:j_idt45")).click();
+	}
+
+	@When("^cashier enters (\\d+)\\.(\\d+) in the payment dialog$")
+	public void cashier_enters_in_the_payment_dialog(int arg1, int arg2) throws Throwable {
+		driver.findElement(By.id("frmDlgPayment:givenInput")).clear();
+		driver.findElement(By.id("frmDlgPayment:givenInput")).sendKeys("10.0");
+		driver.findElement(By.id("frmDlgPayment:j_idt40")).click();
+	}
+
+	@Then("^the article is removed from the list$")
+	public void the_article_is_removed_from_the_list() throws Throwable {
+		driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+		boolean isEmpty = driver.findElements(By.id("cash:j_idt24:0:j_idt28")).isEmpty();
+		driver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
+		if (isEmpty) {
+			return;
+		}
+		throw new Throwable();
+	}
 }
