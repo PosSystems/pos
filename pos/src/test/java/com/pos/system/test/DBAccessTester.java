ab59a1b0050cb8690be833f3a9e35ee085db1925
schnedo <schnedo@web.de>
2017-04-19 10:19:39 +0200

    updated DBAccessTester to test findbyexample

--- pos/src/test/java/com/pos/system/test/DBAccessTester.java
+++ pos/src/test/java/com/pos/system/test/DBAccessTester.java
@@ -1,5 +1,7 @@
 package com.pos.system.test;
 
+import java.util.ArrayList;
+
 import com.pos.system.dao.TblEmployeeHome;
 import com.pos.system.model.TblEmployee;
 
@@ -12,14 +14,36 @@ public class DBAccessTester {
 
 		testEmployee.setStrGivenName("Max");
 		testEmployee.setStrSurname("Mustermann");
-		testEmployee.setIntId(42);
-
+		testEmployee.setIntId(1);
 		employeeDao.persist(testEmployee);
+
+		testEmployee.setStrGivenName("Hans");
+		testEmployee.setStrSurname("Mustermann");
+		testEmployee.setIntId(2);
+		employeeDao.persist(testEmployee);
+
+		testEmployee.setStrGivenName("Peter");
+		testEmployee.setStrSurname("Mustermann");
+		testEmployee.setIntId(3);
+		employeeDao.persist(testEmployee);
+
+		System.out.println("Find by ID");
 		TblEmployee geTblEmployee = employeeDao.findById(42);
 		if (geTblEmployee != null) {
 			System.out.println(geTblEmployee.getStrSurname());
 		} else {
 			System.out.println("du bist doof");
+		}
+
+		System.out.println("\nFind by example");
+		TblEmployee exampleEmployee = new TblEmployee();
+		// exampleEmployee.setStrGivenName("Hans");
+		exampleEmployee.setStrSurname("Mustermann");
+		// employeeDao.persist(exampleEmployee);
+
+		ArrayList<TblEmployee> exampleEmployees = (ArrayList<TblEmployee>) employeeDao.findByExample(exampleEmployee);
+		for (TblEmployee tblEmployee : exampleEmployees) {
+			System.out.println(tblEmployee.getStrGivenName() + ' ' + tblEmployee.getStrSurname());
 		}
 	}
 
