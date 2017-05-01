76a1bbaeabd1410ea39f8801e080769a3c6b84fe
schnedo <schnedo@web.de>
2017-05-01 05:27:47 +0200

    implemented integration sonarqube to maven project

--- pos/src/main/java/com/pos/system/dao/DBAccess.java
+++ pos/src/main/java/com/pos/system/dao/DBAccess.java
@@ -4,8 +4,6 @@ import static org.hibernate.criterion.Example.create;
 
 import java.util.List;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.hibernate.LockMode;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -13,7 +11,7 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class DBAccess<ModelClass> {
-	private static final Log log = LogFactory.getLog(DBAccess.class);
+	// private static final Log log = LogFactory.getLog(DBAccess.class);
 
 	private final SessionFactory sessionFactory = getSessionFactory();
 
@@ -30,83 +28,84 @@ public class DBAccess<ModelClass> {
 	}
 
 	public void persist(ModelClass transientInstance) {
-		log.debug("persisting " + transientInstance.toString() + " instance");
+		// log.debug("persisting " + transientInstance.toString() + "
+		// instance");
 		try {
 			Session session = sessionFactory.getCurrentSession();
 			Transaction transaction = session.beginTransaction();
 			session.persist(transientInstance);
 			transaction.commit();
 			session.close();
-			log.debug("persist successful");
+			// log.debug("persist successful");
 		} catch (RuntimeException re) {
-			log.error("persist failed", re);
+			// log.error("persist failed", re);
 			throw re;
 		}
 	}
 
 	public void attachDirty(ModelClass instance) {
-		log.debug("attaching dirty " + instance.toString() + " instance");
+		// log.debug("attaching dirty " + instance.toString() + " instance");
 		try {
 			Session session = sessionFactory.getCurrentSession();
 			Transaction transaction = session.beginTransaction();
 			session.saveOrUpdate(instance);
 			transaction.commit();
 			session.close();
-			log.debug("attach successful");
+			// log.debug("attach successful");
 		} catch (RuntimeException re) {
-			log.error("attach failed", re);
+			// log.error("attach failed", re);
 			throw re;
 		}
 	}
 
 	public void attachClean(ModelClass instance) {
-		log.debug("attaching clean ModelClass instance");
+		// log.debug("attaching clean ModelClass instance");
 		try {
 			Session session = sessionFactory.getCurrentSession();
 			Transaction transaction = session.beginTransaction();
 			session.lock(instance, LockMode.NONE);
 			transaction.commit();
 			session.close();
-			log.debug("attach successful");
+			// log.debug("attach successful");
 		} catch (RuntimeException re) {
-			log.error("attach failed", re);
+			// log.error("attach failed", re);
 			throw re;
 		}
 	}
 
 	public void delete(ModelClass persistentInstance) {
-		log.debug("deleting ModelClass instance");
+		// log.debug("deleting ModelClass instance");
 		try {
 			Session session = sessionFactory.getCurrentSession();
 			Transaction transaction = session.beginTransaction();
 			session.delete(persistentInstance);
 			transaction.commit();
 			session.close();
-			log.debug("delete successful");
+			// log.debug("delete successful");
 		} catch (RuntimeException re) {
-			log.error("delete failed", re);
+			// log.error("delete failed", re);
 			throw re;
 		}
 	}
 
 	public ModelClass merge(ModelClass detachedInstance) {
-		log.debug("merging ModelClass instance");
+		// log.debug("merging ModelClass instance");
 		try {
 			Session session = sessionFactory.getCurrentSession();
 			Transaction transaction = session.beginTransaction();
 			ModelClass result = (ModelClass) session.merge(detachedInstance);
 			transaction.commit();
 			session.close();
-			log.debug("merge successful");
+			// log.debug("merge successful");
 			return result;
 		} catch (RuntimeException re) {
-			log.error("merge failed", re);
+			// log.error("merge failed", re);
 			throw re;
 		}
 	}
 
 	public ModelClass findById(int id) {
-		log.debug("getting ModelClass instance with id: " + id);
+		// log.debug("getting ModelClass instance with id: " + id);
 		try {
 			Session session = sessionFactory.getCurrentSession();
 			Transaction transaction = session.beginTransaction();
@@ -114,19 +113,19 @@ public class DBAccess<ModelClass> {
 			transaction.commit();
 			session.close();
 			if (instance == null) {
-				log.debug("get successful, no instance found");
+				// log.debug("get successful, no instance found");
 			} else {
-				log.debug("get successful, instance found");
+				// log.debug("get successful, instance found");
 			}
 			return instance;
 		} catch (RuntimeException re) {
-			log.error("get failed", re);
+			// log.error("get failed", re);
 			throw re;
 		}
 	}
 
 	public List<ModelClass> findByExample(ModelClass instance) {
-		log.debug("finding ModelClass instance by example");
+		// log.debug("finding ModelClass instance by example");
 		try {
 			Session session = sessionFactory.getCurrentSession();
 			Transaction transaction = session.beginTransaction();
@@ -134,10 +133,11 @@ public class DBAccess<ModelClass> {
 					.add(create(instance)).list();
 			transaction.commit();
 			session.close();
-			log.debug("find by example successful, result size: " + results.size());
+			// log.debug("find by example successful, result size: " +
+			// results.size());
 			return results;
 		} catch (RuntimeException re) {
-			log.error("find by example failed", re);
+			// log.error("find by example failed", re);
 			throw re;
 		}
 	}
